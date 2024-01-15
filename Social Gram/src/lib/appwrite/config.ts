a09614c95b0ef49161d1cf10d446e44395e003bf
Raghav8810 <raghavsumit630@gmail.com>
2024-01-15 22:00:02 +0530

    changes

--- Social Gram/src/lib/appwrite/config.ts
+++ Social Gram/src/lib/appwrite/config.ts
@@ -9,28 +9,22 @@ import {Client, Account, Databases, Storage,Avatars} from 'appwrite';
 //   }
 
 export const appwriteConfig = {
-    URL: "https://cloud.appwrite.io/v1",
-    projectId: "65a02d3da4094a0a8010",
-    databaseId: "65a1657191cff4b266fd",
-    storageId:"65a16506114780702a22",
-    userCollectionId: "65a1666dd1649f78562c",
-    postCollectionId: "65a165ee6800863ff9ff",
-    savesCollectionId: "65a166aa4dbfa56c9741",
+   
 
-    // url: import.meta.env.VITE_APPWRITE_URL ,
-    // projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    // databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    // storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-    // userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-    // postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-    // savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+    url: import.meta.env.VITE_APPWRITE_URL ,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 
 }
 
 
 export const client = new Client();
 
-client.setEndpoint(appwriteConfig.URL) 
+client.setEndpoint(appwriteConfig.url) 
 client.setProject(appwriteConfig.projectId) 
 //client.setEndpoint(appwriteConfig.url);
 //client.setProject(appwriteConfig.projectId);
